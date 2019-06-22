7b6317aa0216133e57ff98ce082c0ca5b0127b4d
Rodrigo Lemos <usoallstar@gmail.com>
2019-06-22 05:23:44 -0300

    banco de dados add

---
+++ app/src/main/java/com/example/listadetarefas/helper/DBHelper.java
@@ -0,0 +1,35 @@
+package com.example.listadetarefas.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static int VERSION = 1;
+    public static String NOME_DB="DB_TAREFAS";
+    public static String TABELA_TAREFAS = "tarefas";
+    public DBHelper(Context context) {
+        super(context, NOME_DB, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE IF NOT EXISTIS " + TABELA_TAREFAS
+                + "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " nome TEXT NOT NULL ); ";
+
+        try {
+            db.execSQL(sql);
+            Log.i("INFO DB","Sucesso ao criar tabela ");
+        }catch (Exception e){
+            Log.i("INFO DB", "Erro ao criar a tabela" + e.getMessage());
+        }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
