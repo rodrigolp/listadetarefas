961d384387a3b4f7a44d59284bdf036f02bbab76
Rodrigo Lemos <usoallstar@gmail.com>
2019-06-22 04:48:01 -0300

    menu adicionado

--- app/src/main/java/com/example/listadetarefas/activity/adicionar_tarefa.java
+++ app/src/main/java/com/example/listadetarefas/activity/adicionar_tarefa.java
@@ -2,6 +2,8 @@ package com.example.listadetarefas.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.listadetarefas.R;
 
@@ -11,5 +13,22 @@ public class adicionar_tarefa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.itemSalvar;
+
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
